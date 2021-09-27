588812ab76e249bab3704fe0da348740891f4312
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2021-09-27 13:35:16 +0200

    Chart is made
    The chart is made, and the data is already subscribed to Subject.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {SettingsComponent} from "./settings/settings.component";
+import {TempGraphComponent} from "./temp-graph/temp-graph.component";
 
 const routes: Routes = [
-  { path: 'tempGraph', component: DashboardComponent },
+  { path: 'tempGraph', component: TempGraphComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
--- src/app/temp-graph/temp-graph.component.ts
+++ src/app/temp-graph/temp-graph.component.ts
@@ -1,4 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {
+  Chart,
+  BarElement,
+  BarController,
+  CategoryScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  LinearScale,
+  LineController,
+  PointElement,
+  LineElement,
+  PolarAreaController,
+  RadialLinearScale,
+  ArcElement,
+  RadarController
+} from "chart.js";
+import {async, Subject} from "rxjs";
+declare var $:  any;
 
 @Component({
   selector: 'app-temp-graph',
@@ -6,10 +27,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./temp-graph.component.scss']
 })
 export class TempGraphComponent implements OnInit {
+  @ViewChild('chart') chart: any;
 
+  asyncData: Subject<any> = new Subject<any>();
+  asyncData$ = this.asyncData.asObservable();
+
+  bars!: any;
+  colorArray: any;
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  ionViewWillEnter() {
+    this.createBarChart()
+    this.gettingAsyncConnection();
+  }
+
+
+  private createBarChart() {
+    Chart.register(BarElement, BarController, CategoryScale, RadarController, LineController, PolarAreaController, RadialLinearScale, ArcElement, Decimation, Filler, Legend, Title, Tooltip, LinearScale, LineElement, LineController, PointElement);
+    this.bars = new Chart(this.chart.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['Red','Blue', 'Green'],
+        datasets: [{
+          label: 'Temperature in Celsius',
+          backgroundColor: '#ddee44', // array should have same number of elements as number of dataset
+          borderColor: '#ddee44',// array should have same number of elements as number of dataset
+          data: [1,0.70,0.25],
+          fill: 'origin',
+         borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          }
+        }
+      }
+    });
+  }
+
+  click(){
+    this.asyncData.next({
+      date: new Date().toDateString(),
+      temp: Math.random()
+    });
+  }
+
+  addDataToChart(data: any,label: string){
+
+    this.bars.config._config.data.labels.push(label);
+    this.bars.config._config.data.datasets[0].data.push(data);
+    this.bars.update();
+  }
+
+  gettingAsyncConnection(){
+    this.asyncData$.subscribe(resp => this.addDataToChart(resp.temp, resp.date));
+  }
 }
