cbb5b2ed526878112bdca7690c7794b4bfe7099a
Thorbjørn Damkjær <thsd@cloudfactory.dk>
2021-10-06 15:27:35 +0200

    s

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -8,7 +8,7 @@ import {environment} from "../../environments/environment";
   providedIn: 'root'
 })
 export class SettingsService {
-  api = environment + 'settings'
+  api = environment + '/settings'
   constructor(private client: HttpClient) { }
 
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://super-cheese-io.herokuapp.com/',
-  socketUrl: 'https://super-cheese-io.herokuapp.com/'
+  apiUrl: 'https://super-cheese-io.herokuapp.com',
+  socketUrl: 'https://super-cheese-io.herokuapp.com'
 };
